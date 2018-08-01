a206aff94a6e9eea853c4ca35aa274d8e4f1087a
Alex K <flskif@gmail.com>
2018-08-01 16:20:06 +0300

    Implement streaming data from stdin

--- app/metar/Main.hs
+++ app/metar/Main.hs
@@ -1,11 +1,16 @@
 module Main where
 
+import           System.Console.ANSI                      ( clearFromCursorToScreenEnd
+                                                          , cursorUpLine
+                                                          )
 import           Control.Applicative                      ( (<**>)
                                                           , (<|>)
                                                           )
 import           Options.Applicative                      ( Parser
                                                           , ParserInfo
-                                                          , execParser
+                                                          , customExecParser
+                                                          , showHelpOnError
+                                                          , prefs
                                                           , info
                                                           , helper
                                                           , fullDesc
@@ -18,36 +23,85 @@ import           Options.Applicative                      ( Parser
                                                           )
 import           Data.Attoparsec.ByteString.Char8         ( parseOnly )
 import           Data.ByteString                          ( ByteString )
+import qualified Data.ByteString.Char8         as C8
+import           Conduit                                  ( (.|)
+                                                          , runConduit
+                                                          , mapC
+                                                          )
+import           Data.Conduit.Combinators                 ( stdin
+                                                          , mapME
+                                                          , foldME
+                                                          )
+import qualified Data.Map.Strict               as M
 import           METAR.Parsers                            ( metarParser )
-import           METAR.Types                              ( prettyMETAR )
+import           METAR.Types                              ( METAR
+                                                          , Report
+                                                          , prettyMETAR
+                                                          , addMETAR
+                                                          , prettyReport
+                                                          )
 
-data Options
+data Mode
   = METARInput ByteString
   | StreamSTDIN
   deriving (Show)
 
-opts :: ParserInfo Options
-opts = info (optsParser <**> helper)
+opts :: ParserInfo Mode
+opts = info (modeParser <**> helper)
             (fullDesc <> header "METAR records parsing and reporting")
 
-optsParser :: Parser Options
-optsParser = metarInput <|> streamStdin
+modeParser :: Parser Mode
+modeParser = metarInput <|> streamStdin
  where
-  metarInput  = METARInput <$> strArgument (metavar "METAR record")
+  metarInput  = METARInput <$> strArgument (metavar "METAR")
   streamStdin = flag' StreamSTDIN (long "stdin" <> help "Read from stdin")
 
-parseAndPrintMETAR :: ByteString -> IO ()
-parseAndPrintMETAR input =
+-- fail early if there is parsing error
+parseMETAR :: ByteString -> IO METAR
+parseMETAR input =
   case parseOnly metarParser input of
-    Right metar -> putStrLn $ prettyMETAR metar
-    Left  e     -> do
-      putStrLn "Failed to parse METAR record"
-      print e
+    Right metar -> return metar
+    Left e      -> fail $ unlines
+      ["Failed to parse METAR record:"
+      , show input
+      , show e
+      ]
+
+processMETAR :: Report -> METAR -> IO Report
+processMETAR report metar = do
+  let newReport = addMETAR report metar
+
+  clearPreviousReport report
+  printReport newReport
+
+  return newReport
+
+clearPreviousReport :: Report -> IO ()
+clearPreviousReport report =
+  if reportSize == 0
+    then return ()
+    else do
+      cursorUpLine $ 1 + reportSize -- 1 for "Total" line
+      clearFromCursorToScreenEnd
+  where
+    reportSize = M.size report
+
+printMETAR :: METAR -> IO ()
+printMETAR = putStrLn . prettyMETAR
+
+printReport :: Report -> IO ()
+printReport = putStr . prettyReport
 
 main :: IO ()
 main = do
-  o <- execParser opts
-  case o of
-    METARInput input -> parseAndPrintMETAR input
-    StreamSTDIN -> putStrLn "streaming from stdin is not implemented yet"
-
+  mode <- customExecParser (prefs showHelpOnError) opts
+  case mode of
+    METARInput input -> parseMETAR input >>= printMETAR
+    StreamSTDIN      -> do
+      report <- runConduit
+         $ stdin
+        .| mapC C8.lines
+        .| mapME parseMETAR
+        .| foldME processMETAR M.empty
+      clearPreviousReport report
+      printReport report
--- src/METAR/Types.hs
+++ src/METAR/Types.hs
@@ -8,7 +8,11 @@ module METAR.Types
   , mkTimestamp
   , WindInfo
   , mkWindInfo
+  , WindSpeed
   , WindSpeedUnit(KT, MPS)
+  , Report
+  , prettyReport
+  , addMETAR
   )
 where
 
@@ -17,10 +21,12 @@ import           Data.List                                ( intercalate )
 import           Data.Word                                ( Word )
 import           Data.Char                                ( isUpper )
 import           Text.Printf                              ( printf )
+import           Data.Semigroup                           ( Semigroup )
+import qualified Data.Map.Strict               as M
 
 data METAR = METAR ICAOCode Timestamp WindInfo deriving (Eq, Show)
 
-newtype ICAOCode = ICAOCode String deriving (Eq, Show)
+newtype ICAOCode = ICAOCode String deriving (Eq, Show, Ord)
 
 data Timestamp = Timestamp Day Hours Minutes deriving (Eq, Show)
 newtype Day = Day Word deriving (Eq, Show)
@@ -29,24 +35,21 @@ newtype Minutes = Minutes Word deriving (Eq, Show)
 
 data WindInfo = WindInfo WindDirection WindSpeed (Maybe WindGusts) deriving (Eq, Show)
 newtype WindDirection = WindDirection Word deriving (Eq, Show)
-newtype WindSpeed = WindSpeed Word deriving (Eq, Show)
+newtype WindSpeed = WindSpeed { unwindSpeed :: Word } deriving (Eq, Show)
 newtype WindGusts = WindGusts Word deriving (Eq, Show)
 data WindSpeedUnit = KT | MPS
 
-prettyMETAR :: METAR -> String
-prettyMETAR (METAR (ICAOCode i) t w) =
-  i ++ ": " ++ prettyTimestamp t ++ ", " ++ prettyWindInfo w
- where
-  prettyTimestamp (Timestamp (Day d) (Hours h) (Minutes m)) =
-    printf "day %02d, time %02d:%02d" d h m
-  prettyWindInfo (WindInfo d s g) = (intercalate ", " . catMaybes)
-    [ Just $ prettyWindDirection d
-    , Just $ prettyWindSpeed s
-    , prettyWindGusts <$> g
-    ]
-  prettyWindDirection (WindDirection d) = printf "wind from %d°" d
-  prettyWindSpeed (WindSpeed s)         = printf "speed %d m/s" s
-  prettyWindGusts (WindGusts g)         = printf "gusts up to %d m/s" g
+type Report = M.Map ICAOCode LocationReport
+
+data LocationReport = LocationReport
+  { numRecords   :: Word
+  , lastSpeed    :: WindSpeed
+  , averageSpeed :: WindSpeed
+  } deriving (Show)
+
+--
+-- Smart constructors
+--
 
 mkICAOCode :: String -> Either String ICAOCode
 mkICAOCode code
@@ -78,4 +81,60 @@ mkMetar
   -> Either String METAR
 mkMetar icaoCode timestamp windInfo =
   METAR <$> icaoCode <*> timestamp <*> Right windInfo
+
+--
+-- Reporting
+--
+
+instance Semigroup LocationReport where
+  r1 <> r2 = LocationReport
+    { numRecords   = nr1 + nr2
+    , lastSpeed    = lastSpeed r1
+    , averageSpeed = WindSpeed $ quot (ar1 * nr1 + ar2 * nr2) (nr1 + nr2)
+    }
+    where
+      nr1 = numRecords r1
+      nr2 = numRecords r2
+      ar1 = unwindSpeed $ averageSpeed r1
+      ar2 = unwindSpeed $ averageSpeed r2
+
+addMETAR :: Report -> METAR -> Report
+addMETAR report (METAR icao _ (WindInfo _ speed _)) =
+  M.insertWith (<>) icao
+    LocationReport {numRecords = 1, lastSpeed = speed, averageSpeed = speed}
+    report
+
+totalRecords :: Report -> Word
+totalRecords = M.foldl' add 0
+  where
+    add acc r = acc + numRecords r
+
+--
+-- Pretty-printing
+--
+
+prettyReport :: Report -> String
+prettyReport r = unlines $ (prettyRecord <$> M.toList r) ++ [ total ]
+  where
+    prettyRecord (ICAOCode icao, locationReport) =
+      printf "% 5s: average %03d m/s, last %03d m/s" icao avgSpeed speed
+      where
+        speed = unwindSpeed $ lastSpeed locationReport
+        avgSpeed = unwindSpeed $ averageSpeed locationReport
+    total = printf "Total: %d" $ totalRecords r
+
+prettyMETAR :: METAR -> String
+prettyMETAR (METAR (ICAOCode i) t w) =
+  i ++ ": " ++ prettyTimestamp t ++ ", " ++ prettyWindInfo w
+ where
+  prettyTimestamp (Timestamp (Day d) (Hours h) (Minutes m)) =
+    printf "day %02d, time %02d:%02d" d h m
+  prettyWindInfo (WindInfo d s g) = (intercalate ", " . catMaybes)
+    [ Just $ prettyWindDirection d
+    , Just $ prettyWindSpeed s
+    , prettyWindGusts <$> g
+    ]
+  prettyWindDirection (WindDirection d) = printf "wind from %d°" d
+  prettyWindSpeed (WindSpeed s)         = printf "speed %d m/s" s
+  prettyWindGusts (WindGusts g)         = printf "gusts up to %d m/s" g
 
